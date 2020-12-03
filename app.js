139aaeade6619bcbda4e256d6cb90ce633d7bf82
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-12-02 20:51:50 -0500

    Updated dependencies, created gitignore

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ function addEmployee() {
 function viewEmployees() {
     let query = "SELECT * FROM employee";
     connection.query(query, function (err, res) {
+        if (err) throw err;
         console.table(res);
         restartApp();
     });
